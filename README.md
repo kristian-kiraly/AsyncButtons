b4d6e674a7ef5225c8da873b89b48905b2a52621
Kristian Kiraly <kristian.kiraly@gmail.com>
2024-07-07 15:26:10 -0700

    Added Custom Loading Overlays

--- README.md
+++ README.md
@@ -73,3 +73,88 @@ class SomeUIViewController: UIViewController {
 }
 ```
 
+Custom Labels (SwiftUI):
+
+```swift
+struct ContentView: View {
+    var body: some View {
+        AsyncButton { completion in
+            Task {
+                do {
+                    try await Task.sleep(nanoseconds: 3_000_000_000)
+                    completion()
+                } catch {
+                    
+                }
+            }
+        } label: {
+            Text("Test")
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(.black)
+                }
+        }
+        .asyncButtonStyle(PinwheelAsyncButtonStyle())
+    }
+}
+
+struct PinwheelAsyncButtonStyle: AsyncButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .overlay {
+                PinwheelAnimationView()
+                    .padding(10)
+                    .opacity(configuration.isLoading ? 1 : 0)
+                    .animation(.default, value: configuration.isLoading)
+            }
+    }
+}
+
+
+struct PinwheelAnimationView: View {
+    @State private var angle: Double = 0
+    var body: some View {
+        Circle()
+            .fill(AngularGradient(colors: [.red, .orange, .yellow, .green, .blue, .purple, .red], center: .center))
+            .rotationEffect(.degrees(angle))
+            .onAppear {
+                withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)) {
+                    angle = 360
+                }
+            }
+    }
+}
+
+```
+
+Custom Labels (UIKit):
+
+```swift
+
+
+class BlankViewController: UIViewController {
+
+    ...
+
+    @IBAction func press(sender: UIButton) {
+        let blue = UIView(frame: .init(origin: .zero, size: sender.frame.size))
+        blue.backgroundColor = .blue
+        let spinner = UIActivityIndicatorView(style: .medium)
+        spinner.startAnimating()
+        spinner.color = .green
+        spinner.center = blue.center
+        blue.addSubview(spinner)
+        sender.performAsyncAction(customOverlay: blue) { completion in
+            Task {
+                do {
+                    try await Task.sleep(nanoseconds: 3_000_000_000)
+                    completion()
+                } catch {
+                    
+                }
+            }
+        }
+    }
+}
+```
--- Sources/AsyncButtons/AsyncButtons.swift
+++ Sources/AsyncButtons/AsyncButtons.swift
@@ -4,17 +4,17 @@
 import SwiftUI
 
 public extension UIButton {
-    func performAsyncAction(action: @escaping () async -> ()) {
+    func performAsyncAction(customOverlay: UIView? = nil, action: @escaping () async -> ()) {
         Task {
-            startLoading()
+            startLoading(customOverlay: customOverlay)
             await action()
             endLoading()
         }
     }
     
-    func performAsyncAction(action: @escaping (@escaping () -> ()) -> ()) {
+    func performAsyncAction(customOverlay: UIView? = nil, action: @escaping (@escaping () -> ()) -> ()) {
         Task {
-            startLoading()
+            startLoading(customOverlay: customOverlay)
             await withCheckedContinuation { continuation in
                 action {
                     continuation.resume()
@@ -24,38 +24,43 @@ public extension UIButton {
         }
     }
     
-    private var spinnerTag: Int {
+    private var loadingOverlayTag: Int {
         5917737
     }
     
-    private func startLoading() {
+    private func startLoading(customOverlay: UIView? = nil) {
         self.isUserInteractionEnabled = false
         
-        let spinner = UIActivityIndicatorView(style: .medium)
-        spinner.translatesAutoresizingMaskIntoConstraints = false
+        let loadingOverlay: UIView
+        if let customOverlay {
+            loadingOverlay = customOverlay
+        } else {
+            let spinner = UIActivityIndicatorView(style: .medium)
+            spinner.translatesAutoresizingMaskIntoConstraints = false
+            spinner.color = .white
+            spinner.startAnimating()
+            loadingOverlay = spinner
+        }
         
-        spinner.tag = spinnerTag
+        loadingOverlay.tag = loadingOverlayTag
         
-        spinner.alpha = 0
-        spinner.color = .white
+        loadingOverlay.alpha = 0
         
-        self.addSubview(spinner)
+        self.addSubview(loadingOverlay)
         
         NSLayoutConstraint.activate([
-            spinner.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            spinner.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            loadingOverlay.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            loadingOverlay.centerYAnchor.constraint(equalTo: self.centerYAnchor),
         ])
         
         UIView.animate(withDuration: 0.35) {
             self.alpha = self.alpha / 2
-            spinner.alpha = 1
+            loadingOverlay.alpha = 1
         }
-        
-        spinner.startAnimating()
     }
     
     private func endLoading() {
-        let spinner = self.subviews.last(where: {$0.tag == self.spinnerTag})
+        let spinner = self.subviews.last(where: {$0.tag == self.loadingOverlayTag})
         
         UIView.animate(withDuration: 0.35) {
             self.alpha = self.alpha * 2
@@ -73,6 +78,8 @@ public struct AsyncButton<Label: View>: View {
     
     @State private var isLoading = false
     private let externalIsLoading: Bool
+    
+    @Environment(\.asyncButtonStyle) private var style: any AsyncButtonStyle
     
     public init(action: @escaping () async -> (), @ViewBuilder label: @escaping () -> Label) {
         self.action = action
@@ -122,22 +129,83 @@ public struct AsyncButton<Label: View>: View {
                 isLoading = false
             }
         } label: {
-            ZStack {
-                label()
-                    .opacity(isLoading ? 0.5 : 1)
-                Group {
-                    if #available(iOS 15.0, *) {
-                        ProgressView()
-                            .tint(.white)
-                    } else {
-                        ProgressView()
-                            .accentColor(.white)
-                    }
-                }
-                .opacity(isLoading ? 1 : 0)
-            }
-            .animation(.default, value: isLoading)
+            AnyView(
+                style.makeBody(configuration:.init(
+                    isPressed: false,
+                    isLoading: isLoading,
+                    label: AnyView(label()))))
         }
         .disabled(isLoading)
+    }
+}
+
+public protocol AsyncButtonStyle {
+    associatedtype Body: View
+    func makeBody(configuration: Self.Configuration) -> Body
+    
+    typealias Configuration = AsyncButtonStyleConfiguration
+}
+
+public struct AsyncButtonStyleConfiguration {
+    let isPressed: Bool
+    let isLoading: Bool
+    let label: AnyView
+    
+    public init(isPressed: Bool, isLoading: Bool, label: AnyView) {
+        self.isPressed = isPressed
+        self.isLoading = isLoading
+        self.label = label
+    }
+}
+
+public struct DefaultAsyncButtonStyle: AsyncButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            configuration.label
+                .opacity(configuration.isLoading ? 0.5 : 1)
+            Group {
+                if #available(iOS 15.0, *) {
+                    ProgressView()
+                        .tint(.white)
+                } else {
+                    ProgressView()
+                        .accentColor(.white)
+                }
+            }
+            .opacity(configuration.isLoading ? 1 : 0)
+        }
+        .animation(.default, value: configuration.isLoading)
+    }
+}
+
+public extension View {
+    func asyncButtonStyle<S: AsyncButtonStyle>(_ style: S) -> some View {
+        self
+            .environment(\.asyncButtonStyle, style)
+    }
+}
+
+public struct AsyncButtonStyleKey: EnvironmentKey {
+    public static let defaultValue: any AsyncButtonStyle = DefaultAsyncButtonStyle()
+}
+
+public extension EnvironmentValues {
+    var asyncButtonStyle: any AsyncButtonStyle {
+        get { self[AsyncButtonStyleKey.self] }
+        set { self[AsyncButtonStyleKey.self] = newValue }
+    }
+}
+
+public struct AnyAsyncButtonStyle: AsyncButtonStyle {
+    private let _makeBody: (Configuration) -> AnyView
+    
+    public init<S: AsyncButtonStyle>(_ style: S) {
+        self._makeBody = { configuration in
+            AnyView(style.makeBody(configuration: configuration))
+        }
+    }
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        _makeBody(configuration)
     }
 }
